deaad2e522b9602e7d84c46a6845519929c779dc
Andrey Ten <andrey.ten.evg@gmail.com>
2023-02-15 21:06:44 +0600

    statuc added

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
@@ -12,3 +14,8 @@ urlpatterns = [
 handler404 = 'core.views.page_not_found'
 handler403 = 'core.views.csrf_failure'
 handler500 = 'core.views.server_error'
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
